dee8c4413c1ecd700890bd27eea7bee9424d883f
Dan <doates625@gmail.com>
2019-10-18 11:14:05 -0700

    Moved description to README

--- SlewLimiter.h
+++ SlewLimiter.h
@@ -1,22 +1,7 @@
 /**
  * @file SlewLimiter.h
- * @brief Class for implementing asymmetric slew limiting.
+ * @brief Class for implementing asymmetric slew limiting
  * @author Dan Oates (WPI Class of 2020)
- * 
- * This class is used for limiting the rate of change of a value with units 'U'
- * with respect to a time unit 'T'. It assumes a constant sample rate provided
- * in the constructor. Rate limits should be given in units of [U/T] and the
- * sample rate should be given in units of [1/T].
- * 
- * For example, if slew limiting a temperature setpoint in units of
- * [degF / minute], the sample rate should be provided in units of
- * [samples / minute].
- * 
- * On the first frame after construction or reset, the input is assigned
- * directly to the output.
- * 
- * Dependencies:
- * - CppUtil: https://github.com/doates625/CppUtil.git
  */
 #pragma once
 
